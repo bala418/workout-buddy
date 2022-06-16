2c2a69648bf9b760e367efa1a69eb0db9913e69d
bala418 <balavtwo@gmail.com>
2022-06-16 19:46:14 +0530

    setup express router and api routes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const workoutRouter = require("./routes/workouts");
+// express app
 const app = express();
 
+// middleware
+// used to parse the body of the request
+app.use(express.json());
+// used to parse the url
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
-});
+// use the routes only when the following path is hit
+app.use("/api/workouts", workoutRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000");
