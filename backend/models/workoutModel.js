4ba961dab6bad02723f5990f26d18576bcc3f629
bala418 <balavtwo@gmail.com>
2022-06-20 14:35:43 +0530

    Create mongoose model and schema

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    load: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// creates a new collection called "workouts" in the database
+module.exports = mongoose.model("Workout", workoutSchema);
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Workout = require("../models/workoutModel");
 
 // get all routes
 router.get("/", (req, res) => {
@@ -12,8 +13,19 @@ router.get("/:id", (req, res) => {
 });
 
 // POST a new workout
-router.post("/", (req, res) => {
-  res.json({ message: "Post a new workout" });
+router.post("/", async (req, res) => {
+  const { title, reps, load } = req.body;
+  try {
+    const workout = await Workout.create({
+      title,
+      reps,
+      load,
+    });
+
+    res.status(201).json(workout); 
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 // delete a workout
