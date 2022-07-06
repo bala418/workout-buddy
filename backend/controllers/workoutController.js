c334b149b0c8d3c6f2b456f50db227c86c869126
bala418 <balavtwo@gmail.com>
2022-07-06 21:41:56 +0530

    add error handling

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -33,6 +33,23 @@ const getWorkout = async (req, res) => {
 // POST a new workout
 const createWorkout = async (req, res) => {
   const { title, reps, load } = req.body;
+  let emptyFields = [];
+  if (!title) {
+    emptyFields.push("title");
+  }
+  if (!reps) {
+    emptyFields.push("reps");
+  }
+  if (!load) {
+    emptyFields.push("load");
+  }
+
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ message: "Please fill all the fields", emptyFields });
+  }
+
   try {
     const workout = await Workout.create({
       title,
