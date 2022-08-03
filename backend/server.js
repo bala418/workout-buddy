c017d47c8872973dc151f7b2749ee1dd08f1b481
bala418 <balavtwo@gmail.com>
2022-08-03 18:53:27 +0530

    implement users

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const workoutRouter = require("./routes/workouts");
+const userRoutes = require("./routes/user");
 // express app
 const app = express();
 
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 
 // use the routes only when the following path is hit
 app.use("/api/workouts", workoutRouter);
+app.use("/api/user", userRoutes);
 
 // connect to the database
 mongoose
